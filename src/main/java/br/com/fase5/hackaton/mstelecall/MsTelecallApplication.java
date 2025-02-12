300ea692ad24a6797a10172d00c2cc8cb4d776ec
Adriano Pinheiro <adriano_fiorini@hotmail.com>
2025-02-12 11:52:14 -0300

    Criacao da estrutura do projeto de telechamada

---
+++ src/main/java/br/com/fase5/hackaton/mstelecall/MsTelecallApplication.java
@@ -0,0 +1,13 @@
+package br.com.fase5.hackaton.mstelecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTelecallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTelecallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fase5/hackaton/mstelecall/MsTelecallApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fase5.hackaton.mstelecall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTelecallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
